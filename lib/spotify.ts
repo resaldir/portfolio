21e07566a48f3980394af87a17c98ed47c68ff86
resaldir <resaldi.ramadhan97@gmail.com>
2022-04-26 12:52:19 +0700

    Resume v1.0

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -4,10 +4,6 @@ const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
-<<<<<<< HEAD
-=======
-const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
->>>>>>> dcbb74842473cd3a2b0e3271742e494e4132fdb1
 const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=20`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
@@ -28,19 +24,6 @@ const getAccessToken = async () => {
   return response.json();
 };
 
-<<<<<<< HEAD
-=======
-export const getNowPlaying = async () => {
-  const { access_token } = await getAccessToken();
-
-  return fetch(NOW_PLAYING_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
-};
-
->>>>>>> dcbb74842473cd3a2b0e3271742e494e4132fdb1
 export const getTopTracks = async () => {
   const { access_token } = await getAccessToken();
 
--- pages/top-tracks.tsx
+++ pages/top-tracks.tsx
@@ -4,11 +4,7 @@ import { Paragraph } from "pages/link";
 
 export default function TopTracks() {
   return (
-<<<<<<< HEAD
     <Container title="Top Tracks - Resaldi Resaldi Ramadhan Putra">
-=======
-    <Container title="Top Tracks - Lasha Kakabadze">
->>>>>>> dcbb74842473cd3a2b0e3271742e494e4132fdb1
       <h1>Top Tracks</h1>
       <Paragraph>
         Some of my personal favorite records on Spotify, that I think are
